25d2d6d958ec870014c213d86822a92e07e21d34
NikitaD111 <league@WorthyMini-5.attlocal.net>
2018-03-29 19:00:40 -0700

    finished reverse greeting, module 4 done

--- src/threads/ThreadedReverseGreeting.java
+++ src/threads/ThreadedReverseGreeting.java
@@ -1,12 +1,29 @@
+package threads;
 
 public class ThreadedReverseGreeting {
   //Write a program that creates a thread (Thread 1) that creates another thread (Thread 2); 
   //Thread 2 creates Thread 3; and so on, up to Thread 50. 
   //Each thread should print "Hello from Thread <num>!", 
   //but you should structure your program such that the threads print their greetings in reverse order.
-	
-	for(int i = 0; i<50; i++) {
-		Thread i = new Thread(()->{System.out.println("Hello from Thread " + i+ "!");});
+	public static void main(String[] args) {
+			makeThreads(50);
 		
+			
+			//int threadNum = i;
+			//Thread r = new Thread(()->{System.out.println("Hello from Thread " + threadNum + "!");});
+			
 	}
+	
+	static void makeThreads (int i) {
+		if(i>0) {
+			Thread r = new Thread(()->{makeThreads(i-1);});
+			r.start();
+		}
+		System.out.println("Hello from Thread " + i + "!");
+	}
+	
+
 }
+	
+
+
